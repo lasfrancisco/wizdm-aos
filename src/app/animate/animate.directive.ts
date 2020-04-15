ce69ba808d9adb079564c636dec3730f052fff0c
lasfrancisco <admin@bizdemon.com>
2020-04-15 16:34:46 +0200

    1 file modified

--- src/app/animate/animate.directive.ts
+++ src/app/animate/animate.directive.ts
@@ -12,32 +12,37 @@ import { AnimateService } from './animate.service';
 })
 export class AnimateDirective extends AnimateService {
 
-  constructor(private elref: ElementRef<HTMLElement>, scroll: ScrollDispatcher, readonly viewPort: ViewportRuler, zone: NgZone) { 
-    super(scroll, viewPort, zone); 
-  }
-
+  /** When true instructs the directive to use the element's bounding rect as the animation view */
   @Input() set useElement(value: boolean) { this.element = coerceBooleanProperty(value); }
   private element: boolean = false;
 
+  /** Optional top offset */
   @Input() top: number;
 
+  /** Optional left offset */
   @Input() left: number;
 
+  /** Optional bottom offset */
   @Input() bottom: number;
 
+  /** Optional right offset */
   @Input() right: number;
 
-  // Overrides the viewport with the element's client rect
+  // Overrides the viewport with the element's client rect on request
   protected get viewRect(): ClientRect {
-
+    // Selects between the viewport and the element
     const rt = this.element ? this.elref.nativeElement.getBoundingClientRect() 
                             : this.viewPort.getViewportRect();
-
+    // Applies optional offsets 
     const top = rt.top + coerceNumberProperty(this.top, 0);
     const left = rt.left + coerceNumberProperty(this.left, 0);
     const bottom = rt.bottom + coerceNumberProperty(this.bottom, 0);
     const right = rt.right + coerceNumberProperty(this.right, 0);
-                       
+    // Returns the resulting rect
     return { top, left, bottom, right, height: bottom - top, width: right - left };
+  }
+
+  constructor(private elref: ElementRef<HTMLElement>, readonly viewPort: ViewportRuler, scroll: ScrollDispatcher, zone: NgZone) { 
+    super(scroll, viewPort, zone); 
   }
 }
